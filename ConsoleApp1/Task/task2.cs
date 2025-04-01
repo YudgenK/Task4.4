194d09ddb94d47387c8ca6ccb1492b9e73f5d347
YudgenK <yurashka2170@gmail.com>
2025-04-01 20:25:31 +0300

    Виконано

---
+++ ConsoleApp1/Task/task2.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Task
+{
+    class task2
+    {
+        public void Run()
+        {
+            Console.Write("Введіть тип документа (XML, TXT, DOC): ");
+            string docType = Console.ReadLine().ToUpper();
+
+            AbstractHandler handler;
+
+            if (docType == "XML")
+            {
+                handler = new XMLHandler();
+            }
+            else if (docType == "TXT")
+            {
+                handler = new TXTHandler();
+            }
+            else if (docType == "DOC")
+            {
+                handler = new DOCHandler();
+            }
+            else
+            {
+                handler = null;
+            }
+
+            if (handler != null)
+            {
+                handler.Open();
+                handler.Create();
+                handler.Change();
+                handler.Save();
+            }
+            else
+            {
+                Console.WriteLine("Невідомий тип документа.");
+            }
+        }
+    }
+    
+    abstract class AbstractHandler
+    {
+        public abstract void Open();
+        public abstract void Create();
+        public abstract void Change();
+        public abstract void Save();
+    }
+
+    class XMLHandler : AbstractHandler
+    {
+        public override void Open()
+        {
+            Console.WriteLine("Відкрито XML документ");
+        }
+
+        public override void Create()
+        {
+            Console.WriteLine("Створено XML документ");
+        }
+
+        public override void Change()
+        {
+            Console.WriteLine("Редаговано XML документ");
+        }
+
+        public override void Save()
+        {
+            Console.WriteLine("Збережено XML документ");
+        }
+    }
+
+    class TXTHandler : AbstractHandler
+    {
+        public override void Open()
+        {
+            Console.WriteLine("Відкрито TXT документ");
+        }
+
+        public override void Create()
+        {
+            Console.WriteLine("Створено TXT документ");
+        }
+
+        public override void Change()
+        {
+            Console.WriteLine("Редаговано TXT документ");
+        }
+
+        public override void Save()
+        {
+            Console.WriteLine("Збережено TXT документ");
+        }
+    }
+
+    class DOCHandler : AbstractHandler
+    {
+        public override void Open()
+        {
+            Console.WriteLine("Відкрито DOC документ");
+        }
+
+        public override void Create()
+        {
+            Console.WriteLine("Створено DOC документ");
+        }
+
+        public override void Change()
+        {
+            Console.WriteLine("Редаговано DOC документ");
+        }
+
+        public override void Save()
+        {
+            Console.WriteLine("Збережено DOC документ");
+        }
+    }
+
+   
+}
---
+++ ConsoleApp1/Task/task3.cs
@@ -0,0 +1,88 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Task
+{
+    class task3
+    {
+        public void Run()
+        {
+            Player player = new Player();
+
+            Console.WriteLine("Виберіть режим (1 - Відтворення, 2 - Запис): ");
+            string choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                IPlayable playable = player;
+                playable.Play();
+                playable.Pause();
+                playable.Stop();
+            }
+            else if (choice == "2")
+            {
+                IRecodable recordable = player;
+                recordable.Record();
+                recordable.Pause();
+                recordable.Stop();
+            }
+            else
+            {
+                Console.WriteLine("Невірний вибір.");
+            }
+        }
+
+
+        interface IPlayable
+        {
+            void Play();
+            void Pause();
+            void Stop();
+        }
+
+        interface IRecodable
+        {
+            void Record();
+            void Pause();
+            void Stop();
+        }
+
+        class Player : IPlayable, IRecodable
+        {
+            public void Play()
+            {
+                Console.WriteLine("Відтворення розпочато.");
+            }
+
+            public void Pause()
+            {
+                Console.WriteLine("Програвач на паузі.");
+            }
+
+            public void Stop()
+            {
+                Console.WriteLine("Програвач зупинено.");
+            }
+
+            public void Record()
+            {
+                Console.WriteLine("Запис розпочато.");
+            }
+
+            void IRecodable.Pause()
+            {
+                Console.WriteLine("Запис призупинено.");
+            }
+
+            void IRecodable.Stop()
+            {
+                Console.WriteLine("Запис зупинено.");
+            }
+        }
+    }
+}
